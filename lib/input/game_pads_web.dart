IntensiCode/flame-stardash

Add first cut of game features

lib/input/game_pads_web.dart
@@ -1,0 +1,84 @@
+import 'dart:js_interop';
+import 'dart:js_interop_unsafe';
+
+import 'package:stardash/input/game_pads.dart';
+import 'package:stardash/util/auto_dispose.dart';
+import 'package:stardash/util/log.dart';
+import 'package:supercharged/supercharged.dart';
+import 'package:web/web.dart';
+
+mixin HasGamePads {
+  final detected_game_pads = <String, String>{};
+
+  static final _buttons = <(String, int), bool>{}; // .values.associate((it) => MapEntry(it, false));
+  static final _axes = <(String, int), double>{};
+
+  void tick_game_pads() {
+    final it = window.navigator.getGamepads().toDart;
+    if (it.isEmpty) return;
+    if (it.count((e) => e != null) != detected_game_pads.length) {
+      log_info('clear detected game pads');
+      detected_game_pads.clear();
+    }
+    final was = detected_game_pads.length;
+    for (final gp in it) {
+      if (gp == null) continue;
+      detected_game_pads[gp.id] = gp.id;
+      _tick_game_pad(gp);
+    }
+    if (was != detected_game_pads.length) {
+      log_info('detected game pads: $detected_game_pads');
+    }
+  }
+
+  void _tick_game_pad(Gamepad gp) {
+    final buttons = gp.buttons.toDart;
+    for (var i = 0; i < buttons.length; i++) {
+      final button = buttons[i];
+      if (_buttons[(gp.id, i)] == button.pressed) continue;
+      _buttons[(gp.id, i)] = button.pressed;
+      onSnoop(SnoopType.button, 200 + i, button.value);
+      final gpc = hw_mapping[200 + i];
+      if (gpc != null) onGamePad(gpc, button.value);
+    }
+
+    final axes = gp.axes.toDart;
+    for (final (i, value) in axes.indexed) {
+      final now = value.toDartDouble;
+      if (now == _axes[(gp.id, i)]) continue;
+      _axes[(gp.id, i)] = now;
+      onSnoop(SnoopType.axis, 100 + i, now);
+      final gpc = hw_mapping[100 + i];
+      if (gpc != null) onGamePad(gpc, now);
+    }
+  }
+
+  Disposable observe_gamepads() {
+    final it = window.navigator.getGamepads().toDart;
+    for (final gp in it) {
+      if (gp == null) continue;
+      detected_game_pads[gp.id] = gp.id;
+    }
+    log_info('detected game pads: $detected_game_pads');
+    return Disposable.disposed;
+  }
+
+  void rumble([int duration = 100]) {
+    final it = window.navigator.getGamepads().toDart;
+    if (it.isEmpty) return;
+    for (final gp in it) {
+      final va = gp?.getProperty('vibrationActuator'.toJS);
+      if (va != null) _rumble_va(va, duration);
+    }
+  }
+
+  void _rumble_va(JSAny va, int duration) {
+    final obj = JSObject.fromInteropObject(va);
+    final data = JSObject();
+    data.setProperty('duration'.toJS, duration.toJS);
+    data.setProperty('startDelay'.toJS, 0.toJS);
+    data.setProperty('strongMagnitude'.toJS, 1.toJS);
+    data.setProperty('weakMagnitude'.toJS, 1.toJS);
+    obj.callMethod('playEffect'.toJS, 'dual-rumble'.toJS, data);
+  }
+}

lib/util/bitmap_text.dart
@@ -1,0 +1,84 @@
+import 'dart:ui';
+
+import 'package:flame/components.dart';
+import 'package:stardash/ui/fonts.dart';
+import 'package:stardash/util/bitmap_font.dart';
+
+class BitmapText extends PositionComponent with HasPaint, HasVisibility, Snapshot {
+  final _reference = Vector2.zero();
+
+  final BitmapFont font;
+  final double fontScale;
+  final Anchor _text_anchor;
+
+  bool default_snapshot;
+
+  String _text;
+
+  String get text => _text;
+
+  BitmapText({
+    required String text,
+    required Vector2 position,
+    BitmapFont? font,
+    double scale = 1,
+    Color? tint,
+    this.default_snapshot = true,
+    Anchor anchor = Anchor.topLeft,
+  })  : _text = text,
+        _text_anchor = anchor,
+        font = font ?? mini_font,
+        fontScale = scale {
+    if (tint != null) this.tint(tint);
+    _reference.setFrom(position);
+    this.font.scale = fontScale;
+    _update_position(text);
+  }
+
+  void _update_position(String text) {
+    _text = text;
+
+    font.scale = fontScale;
+    final w = font.lineWidth(_text);
+    final h = font.lineHeight(fontScale);
+    size.setValues(w, h);
+
+    final x = _text_anchor.x * w;
+    final y = _text_anchor.y * h;
+    position.setFrom(_reference);
+    position.x -= x;
+    position.y -= y;
+  }
+
+  set text(String text) => change_text_in_place(text);
+
+  void change_text_in_place(String text) {
+    _text = text;
+    _update_position(text);
+    clearSnapshot();
+  }
+
+  @override
+  set isVisible(bool it) {
+    super.isVisible = it;
+    clearSnapshot();
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+    if (default_snapshot) renderSnapshot = opacity == 1;
+  }
+
+  @override
+  void render(Canvas canvas) {
+    super.render(canvas);
+    font.paint.color = paint.color;
+    font.paint.colorFilter = paint.colorFilter;
+    font.paint.filterQuality = FilterQuality.none;
+    font.paint.isAntiAlias = false;
+    font.paint.blendMode = paint.blendMode;
+    font.scale = fontScale;
+    font.drawString(canvas, 0, 0, _text);
+  }
+}

lib/util/performance.dart
@@ -1,0 +1,76 @@
+import 'dart:ui';
+
+import 'package:flame/components.dart';
+import 'package:flame/text.dart';
+import 'package:flutter/foundation.dart';
+
+class Ticker {
+  Ticker({int ticks = 60}) : _step = 1 / ticks;
+
+  final double _step;
+
+  double _remainder = 0;
+
+  generateTicksFor(double dt, void Function(double) tick) {
+    // for historic reasons i prefer constant ticks... ‾\_('')_/‾
+    dt += _remainder;
+    while (dt >= _step) {
+      tick(_step);
+      dt -= _step;
+    }
+    _remainder = dt;
+  }
+}
+
+class RenderTps<T extends TextRenderer> extends TextComponent with HasVisibility {
+  RenderTps({
+    super.position,
+    super.size,
+    super.scale,
+    super.anchor,
+  }) : super(priority: double.maxFinite.toInt()) {
+    add(fpsComponent);
+  }
+
+  final fpsComponent = FpsComponent();
+
+  @override
+  bool get isVisible => !kReleaseMode;
+
+  @override
+  void update(double dt) => text = '${fpsComponent.fps.toStringAsFixed(0)} TPS';
+}
+
+class RenderFps<T extends TextRenderer> extends TextComponent with HasVisibility {
+  RenderFps({
+    super.position,
+    super.size,
+    super.scale,
+    super.anchor,
+    super.key,
+  }) : super(priority: double.maxFinite.toInt());
+
+  @override
+  bool get isVisible => !kReleaseMode;
+
+  @override
+  void update(double dt) {}
+
+  static const maxSnapshots = 100;
+  final snapshots = <int>[];
+
+  int previousFrame = 0;
+
+  @override
+  void render(Canvas canvas) {
+    super.render(canvas);
+    if (previousFrame > 0) {
+      final delta = DateTime.timestamp().millisecondsSinceEpoch - previousFrame;
+      if (snapshots.length == maxSnapshots) snapshots.removeAt(0);
+      snapshots.add(delta);
+      final average = snapshots.reduce((value, element) => value + element) / snapshots.length;
+      text = '${(1000 / average).toStringAsFixed(0)} FPS';
+    }
+    previousFrame = DateTime.timestamp().millisecondsSinceEpoch;
+  }
+}

lib/voxel/voxel_image.dart
@@ -1,0 +1,110 @@
+import 'dart:io';
+import 'dart:ui';
+
+import 'package:collection/collection.dart';
+import 'package:flame/components.dart';
+import 'package:flame/sprite.dart';
+import 'package:flutter/foundation.dart';
+import 'package:stardash/core/common.dart';
+import 'package:stardash/util/log.dart';
+import 'package:stardash/util/pixelate.dart';
+import 'package:stardash/voxel/voxel_io.dart';
+import 'package:stardash/voxel/voxels.dart';
+
+Image vox_to_image(
+  Voxels voxels, {
+  int dx = 0,
+  int dy = 0,
+  int dz = 0,
+  List<int>? blurred_argb32,
+}) {
+  final blurred = blurred_argb32 ??= [];
+  return pixelate(voxels.width, voxels.height * voxels.depth, (canvas) {
+    final paint = Paint()..style = PaintingStyle.fill;
+    final blur = MaskFilter.blur(BlurStyle.outer, 1);
+    for (var y = dy; y < voxels.height + dy; y++) {
+      for (var z = dz; z < voxels.depth + dz; z++) {
+        for (var x = dx; x < voxels.width + dx; x++) {
+          final yy = voxels.height - 1 - y + dy;
+          final zz = voxels.depth - 1 - z + dz;
+          final xx = voxels.width - 1 - x + dx;
+          final v = voxels.voxels[yy][zz][xx];
+          if (v == 0) continue;
+          final color = voxels.palette[v - 1];
+          paint.color = Color(color);
+          if (blurred.contains(color)) {
+            paint.maskFilter = blur;
+            canvas.drawRect(Rect.fromLTWH(x.toDouble() - 1, y.toDouble() * voxels.depth + z - 1, 3, 3), paint);
+            paint.maskFilter = null;
+          }
+          canvas.drawRect(Rect.fromLTWH(x.toDouble(), y.toDouble() * voxels.depth + z, 1, 1), paint);
+        }
+      }
+    }
+  });
+}
+
+Image vox_to_image_ext(Voxels voxels, {bool Function(Vector3 xyz, int color, Canvas canvas, Paint paint)? on_pixel}) {
+  return pixelate(voxels.width, voxels.height * voxels.depth, (canvas) {
+    final xyz = Vector3.zero();
+    final paint = pixel_paint();
+    for (var y = 0; y < voxels.height; y++) {
+      for (var z = 0; z < voxels.depth; z++) {
+        for (var x = 0; x < voxels.width; x++) {
+          final yy = voxels.height - 1 - y;
+          final zz = voxels.depth - 1 - z;
+          final xx = voxels.width - 1 - x;
+
+          final v = voxels.voxels[yy][zz][xx];
+          if (v == 0) continue;
+
+          final color = voxels.palette[v - 1];
+          paint.color = Color(color);
+
+          xyz.setValues(x + 0, y + 0, z + 0);
+          if (on_pixel?.call(xyz, color, canvas, paint) == true) continue;
+
+          canvas.drawRect(Rect.fromLTWH(xyz.x, xyz.z, 1, 1), paint);
+        }
+      }
+      canvas.translate(0, voxels.depth * 1);
+    }
+  });
+}
+
+Future<SpriteSheet> make_anim(int frames, Future<Image> Function(int) make_frame) async {
+  final images = <Image>[];
+  for (var i = 0; i < frames; i++) {
+    images.add(await make_frame(i));
+  }
+  final width = images.map((it) => it.width + 2).sum;
+  final height = images.map((it) => it.height + 2).max;
+  log_info('Creating animation with $frames frames, full size $width x $height');
+
+  final paint = pixel_paint();
+  final sheet = pixelate(width, height, (canvas) {
+    var x = 0;
+    for (final image in images) {
+      canvas.drawImage(image, Offset(x.toDouble() + 1, 1), paint);
+      x += image.width + 2;
+    }
+  });
+
+  // sheet.toByteData(format: ImageByteFormat.png).then((data) {
+  //   File('anim.png').writeAsBytesSync(Uint8List.view(data!.buffer));
+  // });
+
+  return SpriteSheet.fromColumnsAndRows(image: sheet, columns: frames, rows: 1, spacing: 2, margin: 1);
+}
+
+Future<(Voxels, Image)> vox_image(String name, {int dx = 0, int dy = 0, int dz = 0, List<int>? blurred_argb32}) {
+  return game.assets.readBinaryFile('entities/$name').then((it) {
+    if (it.length >= 2 && it[0] == 0x1f && it[1] == 0x8b) {
+      log_info('Decompressing $name.vox.gz');
+      final data = GZipCodec().decode(it.toList());
+      it = Uint8List.fromList(data);
+    }
+    final voxels = read_vox(it, name);
+    return (voxels, vox_to_image(voxels, dx: dx, dy: dy, dz: dz, blurred_argb32: blurred_argb32));
+  });
+}

IntensiCode/flame-flutteroids

lib/input/game_pads_web.dart
@@ -1,0 +1,84 @@
+import 'dart:js_interop';
+import 'dart:js_interop_unsafe';
+
+import 'package:flutteroids/input/game_pads.dart';
+import 'package:flutteroids/util/auto_dispose.dart';
+import 'package:flutteroids/util/log.dart';
+import 'package:supercharged/supercharged.dart';
+import 'package:web/web.dart';
+
+mixin HasGamePads {
+  final detected_game_pads = <String, String>{};
+
+  static final _buttons = <(String, int), bool>{}; // .values.associate((it) => MapEntry(it, false));
+  static final _axes = <(String, int), double>{};
+
+  void tick_game_pads() {
+    final it = window.navigator.getGamepads().toDart;
+    if (it.isEmpty) return;
+    if (it.count((e) => e != null) != detected_game_pads.length) {
+      log_info('clear detected game pads');
+      detected_game_pads.clear();
+    }
+    final was = detected_game_pads.length;
+    for (final gp in it) {
+      if (gp == null) continue;
+      detected_game_pads[gp.id] = gp.id;
+      _tick_game_pad(gp);
+    }
+    if (was != detected_game_pads.length) {
+      log_info('detected game pads: $detected_game_pads');
+    }
+  }
+
+  void _tick_game_pad(Gamepad gp) {
+    final buttons = gp.buttons.toDart;
+    for (var i = 0; i < buttons.length; i++) {
+      final button = buttons[i];
+      if (_buttons[(gp.id, i)] == button.pressed) continue;
+      _buttons[(gp.id, i)] = button.pressed;
+      onSnoop(SnoopType.button, 200 + i, button.value);
+      final gpc = hw_mapping[200 + i];
+      if (gpc != null) onGamePad(gpc, button.value);
+    }
+
+    final axes = gp.axes.toDart;
+    for (final (i, value) in axes.indexed) {
+      final now = value.toDartDouble;
+      if (now == _axes[(gp.id, i)]) continue;
+      _axes[(gp.id, i)] = now;
+      onSnoop(SnoopType.axis, 100 + i, now);
+      final gpc = hw_mapping[100 + i];
+      if (gpc != null) onGamePad(gpc, now);
+    }
+  }
+
+  Disposable observe_gamepads() {
+    final it = window.navigator.getGamepads().toDart;
+    for (final gp in it) {
+      if (gp == null) continue;
+      detected_game_pads[gp.id] = gp.id;
+    }
+    log_info('detected game pads: $detected_game_pads');
+    return Disposable.disposed;
+  }
+
+  void rumble([int duration = 100]) {
+    final it = window.navigator.getGamepads().toDart;
+    if (it.isEmpty) return;
+    for (final gp in it) {
+      final va = gp?.getProperty('vibrationActuator'.toJS);
+      if (va != null) _rumble_va(va, duration);
+    }
+  }
+
+  void _rumble_va(JSAny va, int duration) {
+    final obj = JSObject.fromInteropObject(va);
+    final data = JSObject();
+    data.setProperty('duration'.toJS, duration.toJS);
+    data.setProperty('startDelay'.toJS, 0.toJS);
+    data.setProperty('strongMagnitude'.toJS, 1.toJS);
+    data.setProperty('weakMagnitude'.toJS, 1.toJS);
+    obj.callMethod('playEffect'.toJS, 'dual-rumble'.toJS, data);
+  }
+}

lib/util/bitmap_text.dart
@@ -1,0 +1,84 @@
+import 'dart:ui';
+
+import 'package:flame/components.dart';
+import 'package:flutteroids/ui/fonts.dart';
+import 'package:flutteroids/util/bitmap_font.dart';
+
+class BitmapText extends PositionComponent with HasPaint, HasVisibility, Snapshot {
+  final _reference = Vector2.zero();
+
+  final BitmapFont font;
+  final double fontScale;
+  final Anchor _text_anchor;
+
+  bool default_snapshot;
+
+  String _text;
+
+  String get text => _text;
+
+  BitmapText({
+    required String text,
+    required Vector2 position,
+    BitmapFont? font,
+    double scale = 1,
+    Color? tint,
+    this.default_snapshot = true,
+    Anchor anchor = Anchor.topLeft,
+  })  : _text = text,
+        _text_anchor = anchor,
+        font = font ?? mini_font,
+        fontScale = scale {
+    if (tint != null) this.tint(tint);
+    _reference.setFrom(position);
+    this.font.scale = fontScale;
+    _update_position(text);
+  }
+
+  void _update_position(String text) {
+    _text = text;
+
+    font.scale = fontScale;
+    final w = font.lineWidth(_text);
+    final h = font.lineHeight(fontScale);
+    size.setValues(w, h);
+
+    final x = _text_anchor.x * w;
+    final y = _text_anchor.y * h;
+    position.setFrom(_reference);
+    position.x -= x;
+    position.y -= y;
+  }
+
+  set text(String text) => change_text_in_place(text);
+
+  void change_text_in_place(String text) {
+    _text = text;
+    _update_position(text);
+    clearSnapshot();
+  }
+
+  @override
+  set isVisible(bool it) {
+    super.isVisible = it;
+    clearSnapshot();
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+    if (default_snapshot) renderSnapshot = opacity == 1;
+  }
+
+  @override
+  void render(Canvas canvas) {
+    super.render(canvas);
+    font.paint.color = paint.color;
+    font.paint.colorFilter = paint.colorFilter;
+    font.paint.filterQuality = FilterQuality.none;
+    font.paint.isAntiAlias = false;
+    font.paint.blendMode = paint.blendMode;
+    font.scale = fontScale;
+    font.drawString(canvas, 0, 0, _text);
+  }
+}

lib/util/performance.dart
@@ -1,0 +1,76 @@
+import 'dart:ui';
+
+import 'package:flame/components.dart';
+import 'package:flame/text.dart';
+import 'package:flutter/foundation.dart';
+
+class Ticker {
+  Ticker({int ticks = 60}) : step = 1.0 / ticks;
+
+  final double step;
+
+  double _remainder = 0;
+
+  generateTicksFor(double dt, void Function(double) tick) {
+    // for historic reasons i prefer constant ticks... ‾\_('')_/‾
+    dt += _remainder;
+    while (dt >= step) {
+      tick(step);
+      dt -= step;
+    }
+    _remainder = dt;
+  }
+}
+
+class RenderTps<T extends TextRenderer> extends TextComponent with HasVisibility {
+  RenderTps({
+    super.position,
+    super.size,
+    super.scale,
+    super.anchor,
+  }) : super(priority: double.maxFinite.toInt()) {
+    add(fpsComponent);
+  }
+
+  final fpsComponent = FpsComponent();
+
+  @override
+  bool get isVisible => !kReleaseMode;
+
+  @override
+  void update(double dt) => text = '${fpsComponent.fps.toStringAsFixed(0)} TPS';
+}
+
+class RenderFps<T extends TextRenderer> extends TextComponent with HasVisibility {
+  RenderFps({
+    super.position,
+    super.size,
+    super.scale,
+    super.anchor,
+    super.key,
+  }) : super(priority: double.maxFinite.toInt());
+
+  @override
+  bool get isVisible => !kReleaseMode;
+
+  @override
+  void update(double dt) {}
+
+  static const maxSnapshots = 100;
+  final snapshots = <int>[];
+
+  int previousFrame = 0;
+
+  @override
+  void render(Canvas canvas) {
+    super.render(canvas);
+    if (previousFrame > 0) {
+      final delta = DateTime.timestamp().millisecondsSinceEpoch - previousFrame;
+      if (snapshots.length == maxSnapshots) snapshots.removeAt(0);
+      snapshots.add(delta);
+      final average = snapshots.reduce((value, element) => value + element) / snapshots.length;
+      text = '${(1000 / average).toStringAsFixed(0)} FPS';
+    }
+    previousFrame = DateTime.timestamp().millisecondsSinceEpoch;
+  }
+}

lib/voxel/voxel_image.dart
@@ -1,0 +1,110 @@
+import 'dart:io';
+import 'dart:ui';
+
+import 'package:collection/collection.dart';
+import 'package:flame/components.dart';
+import 'package:flame/sprite.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutteroids/core/common.dart';
+import 'package:flutteroids/util/log.dart';
+import 'package:flutteroids/util/pixelate.dart';
+import 'package:flutteroids/voxel/voxel_io.dart';
+import 'package:flutteroids/voxel/voxels.dart';
+
+Image vox_to_image(
+  Voxels voxels, {
+  int dx = 0,
+  int dy = 0,
+  int dz = 0,
+  List<int>? blurred_argb32,
+}) {
+  final blurred = blurred_argb32 ??= [];
+  return pixelate(voxels.width, voxels.height * voxels.depth, (canvas) {
+    final paint = Paint()..style = PaintingStyle.fill;
+    final blur = MaskFilter.blur(BlurStyle.outer, 1);
+    for (var y = dy; y < voxels.height + dy; y++) {
+      for (var z = dz; z < voxels.depth + dz; z++) {
+        for (var x = dx; x < voxels.width + dx; x++) {
+          final yy = voxels.height - 1 - y + dy;
+          final zz = voxels.depth - 1 - z + dz;
+          final xx = voxels.width - 1 - x + dx;
+          final v = voxels.voxels[yy][zz][xx];
+          if (v == 0) continue;
+          final color = voxels.palette[v - 1];
+          paint.color = Color(color);
+          if (blurred.contains(color)) {
+            paint.maskFilter = blur;
+            canvas.drawRect(Rect.fromLTWH(x.toDouble() - 1, y.toDouble() * voxels.depth + z - 1, 3, 3), paint);
+            paint.maskFilter = null;
+          }
+          canvas.drawRect(Rect.fromLTWH(x.toDouble(), y.toDouble() * voxels.depth + z, 1, 1), paint);
+        }
+      }
+    }
+  });
+}
+
+Image vox_to_image_ext(Voxels voxels, {bool Function(Vector3 xyz, int color, Canvas canvas, Paint paint)? on_pixel}) {
+  return pixelate(voxels.width, voxels.height * voxels.depth, (canvas) {
+    final xyz = Vector3.zero();
+    final paint = pixel_paint();
+    for (var y = 0; y < voxels.height; y++) {
+      for (var z = 0; z < voxels.depth; z++) {
+        for (var x = 0; x < voxels.width; x++) {
+          final yy = voxels.height - 1 - y;
+          final zz = voxels.depth - 1 - z;
+          final xx = voxels.width - 1 - x;
+
+          final v = voxels.voxels[yy][zz][xx];
+          if (v == 0) continue;
+
+          final color = voxels.palette[v - 1];
+          paint.color = Color(color);
+
+          xyz.setValues(x + 0, y + 0, z + 0);
+          if (on_pixel?.call(xyz, color, canvas, paint) == true) continue;
+
+          canvas.drawRect(Rect.fromLTWH(xyz.x, xyz.z, 1, 1), paint);
+        }
+      }
+      canvas.translate(0, voxels.depth * 1);
+    }
+  });
+}
+
+Future<SpriteSheet> make_anim(int frames, Future<Image> Function(int) make_frame) async {
+  final images = <Image>[];
+  for (var i = 0; i < frames; i++) {
+    images.add(await make_frame(i));
+  }
+  final width = images.map((it) => it.width + 2).sum;
+  final height = images.map((it) => it.height + 2).max;
+  log_info('Creating animation with $frames frames, full size $width x $height');
+
+  final paint = pixel_paint();
+  final sheet = pixelate(width, height, (canvas) {
+    var x = 0;
+    for (final image in images) {
+      canvas.drawImage(image, Offset(x.toDouble() + 1, 1), paint);
+      x += image.width + 2;
+    }
+  });
+
+  // sheet.toByteData(format: ImageByteFormat.png).then((data) {
+  //   File('anim.png').writeAsBytesSync(Uint8List.view(data!.buffer));
+  // });
+
+  return SpriteSheet.fromColumnsAndRows(image: sheet, columns: frames, rows: 1, spacing: 2, margin: 1);
+}
+
+Future<(Voxels, Image)> vox_image(String name, {int dx = 0, int dy = 0, int dz = 0, List<int>? blurred_argb32}) {
+  return game.assets.readBinaryFile('entities/$name').then((it) {
+    if (it.length >= 2 && it[0] == 0x1f && it[1] == 0x8b) {
+      log_info('Decompressing $name.vox.gz');
+      final data = GZipCodec().decode(it.toList());
+      it = Uint8List.fromList(data);
+    }
+    final voxels = read_vox(it, name);
+    return (voxels, vox_to_image(voxels, dx: dx, dy: dy, dz: dz, blurred_argb32: blurred_argb32));
+  });
+}